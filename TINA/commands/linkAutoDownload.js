ac7d3285c0a50735ad5eb7ef2ca7ec08bdc76902
mowsumi12 <164328415+mowsumi12@users.noreply.github.com>
2025-02-14 17:40:25 +0600

    Update and rename linkAutoDownload to linkAutoDownload.js

--- TINA/commands/linkAutoDownload.js
+++ TINA/commands/linkAutoDownload.js
@@ -3,7 +3,7 @@ module.exports = {
     name: "linkAutoDownload",
     version: "1.3.0",
     hasPermssion: 0,
-    credits: "ARIF BABU",
+    credits: "nazrul",
     description:
       "Automatically detects links in messages and downloads the file.",
     commandCategory: "Utilities",
